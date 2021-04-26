282222d88e0764be3abf1656f5a88bff4a5e42eb
jeorgy <jeorgyvtramalho@gmail.com>
2021-04-26 20:49:31 -0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="map">
+    <l-map :center="center" :zoom="zoom">
+      <l-tile-layer :url="url"></l-tile-layer>
+      <l-marker v-for="(marker, i) in markers" :key="i"
+        draggable :lat-lng="marker">
+        <l-tooltip>Olá mundo!</l-tooltip>
+        <l-popup>Eu sou um popup!</l-popup>
+      </l-marker>
+    </l-map>
   </div>
 </template>
 
 <script>
+import { LPopup, LTooltip } from "vue2-leaflet";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    'l-popup': LPopup,
+    'l-tooltip': LTooltip
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 13,
+      center: {lat:-7.142305, lng:-34.8572066},
+      markers: [
+        {lat:-7.162305, lng:-34.8552066},
+        {lat:-7.122305, lng:-34.8572066},
+        {lat:-7.142305, lng:-34.8672066},
+        {lat:-7.148305, lng:-34.8502066},
+      ],
+      variavel_booleana: false
+    };
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .map {
+    height: 100vh;
+    width: 100vw;
+  }
 </style>
